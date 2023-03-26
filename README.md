c0b7cce56f3d66329be0b58aa5ca5d1109c25302
Lukas LukyLuke_ch Zurschmiede <lukas.zurschmiede@ranta.ch>
2023-03-26 15:11:08 +0200

    Combine grassfire::v2 and grassfire::v3 as grassfire::v4

--- README.md
+++ README.md
@@ -11,7 +11,8 @@ Two versions of the Grassfire-Algorithm to show how fast it can be if optimized.
 
 * **v1:** This version loops through all rows and cols and updates the neighbours as soon as it finds a value. This is a 4-Neighborhood calculation.
 * **v2:** In this version the fields which have to be calculated next are cached. So no looping thorugh all rows and columns. This is a 4-Neighborhood calculation.
-* **v3:** This is an 8-Neighborhood calculation and pathfinding absed on the optimized *v2*.
+* **v3:** This is an 8-Neighborhood calculation and pathfinding based on the optimized *v2*.
+* **v4:** This is an 4-Neighborhood calculation (based on *v2*) and a 8-Neighborhood pathfinding (as in *v3*).
 
 ### Usage
 
@@ -32,64 +33,66 @@ $ target/release/pathfinder ROWS COLS START_ROW START_COL END_ROW END_COL [OBSTA
 
 ```
 $ target/release/pathfinder 1000 1000 12 99 800 750
-Create Area: 23.597266ms
-Area 1 invalid: Start or End is inside an obstacle.
-Create Area: 21.790536ms
-Area 2 invalid: Start or End is inside an obstacle.
-Create Area: 28.023848ms
-Area 3 invalid: Start or End is inside an obstacle.
-Create Area: 14.823762ms
+Create Area: 13.234211ms
 Field Size: 1000x1000
 Obstacles:  100 max 100x100
 
-Grassfire-V1 Calc: 38.487738s
-Grassfire-V1 Path-Calculation: 243.118000µs
+Grassfire-V1 Calc: 38.070922s
+Grassfire-V1 Path-Calculation: 222.449000µs
 Grassfire-V1 Path length: 1440
 Saved: grassfire_v1.png
 
-Grassfire-V2 Calc: 99.330146ms
-Grassfire-V2 Path-Calculation: 207.100000µs
+Grassfire-V2 Calc: 97.991937ms
+Grassfire-V2 Path-Calculation: 208.282000µs
 Grassfire-V2 Path length: 1440
 Saved: grassfire_v2.png
 
-Grassfire-V3 Calc: 177.441322ms
-Grassfire-V3 Path-Calculation: 199.515000µs
-Grassfire-V3 Path length: 855
+Grassfire-V3 Calc: 162.971837ms
+Grassfire-V3 Path-Calculation: 179.437000µs
+Grassfire-V3 Path length: 818
 Saved: grassfire_v3.png
+
+Grassfire-V4 Calc: 96.350969ms
+Grassfire-V4 Path-Calculation: 189.376000µs
+Grassfire-V4 Path length: 964
+Saved: grassfire_v4.png
 ```
 
-* *v1* is nearly 400 times slower than *v2* and 215 times slower than *v3*
+* *v1* is nearly 400 times slower than *v2* and 230 times slower than *v3*
 * *v2* is around 40%-50% faster than *v3* becasue it has 4 calculations less than *v3*
 * *v3* has nearly 50% less steps the robot has to move than in *v1* and *v2*
+* *v4* has around the same number of steps for the robot as *v3* but with the calculation speed of *v2*
 
 ### Example: Start bottom right, end top left
 
 ```
 $ target/release/pathfinder 1000 1000 800 750 12 99
-Create Area: 13.838047ms
-Area 1 invalid: Start or End is inside an obstacle.
-Create Area: 12.429355ms
-Area 2 invalid: Start or End is inside an obstacle.
-Create Area: 14.683338ms
+Create Area: 16.536774ms
 Field Size: 1000x1000
 Obstacles:  100 max 100x100
 
-Grassfire-V1 Calc: 34.534503ms
-Grassfire-V1 Path-Calculation: 191.280000µs
+Grassfire-V1 Calc: 31.927361ms
+Grassfire-V1 Path-Calculation: 200.247000µs
 Grassfire-V1 Path length: 1440
 Saved: grassfire_v1.png
 
-Grassfire-V2 Calc: 90.071216ms
-Grassfire-V2 Path-Calculation: 169.519000µs
+Grassfire-V2 Calc: 89.073367ms
+Grassfire-V2 Path-Calculation: 177.133000µs
 Grassfire-V2 Path length: 1440
 Saved: grassfire_v2.png
 
-Grassfire-V3 Calc: 142.868994ms
-Grassfire-V3 Path-Calculation: 155.412000µs
-Grassfire-V3 Path length: 862
+Grassfire-V3 Calc: 131.293040ms
+Grassfire-V3 Path-Calculation: 146.385000µs
+Grassfire-V3 Path length: 808
 Saved: grassfire_v3.png
+
+Grassfire-V4 Calc: 93.812297ms
+Grassfire-V4 Path-Calculation: 100.288000µs
+Grassfire-V4 Path length: 808
+Saved: grassfire_v4.png
 ```
 
-* *v1* is only 2.5 times slower, in most cases it is arount the same speed as *v2*. Under some circumstances it may be that *v1* is even faster because it can update all cells in one run.
+* *v1* is only 2-3 times slower, in most cases it is arount the same speed as *v2*. Under some circumstances it may be that *v1* is even faster because it can update all cells in one run.
 * *v2* is around 40%-50% faster than *v3* becasue it has 4 calculations less than *v3*
 * *v3* has nearly 50% less steps the robot has to move than in *v1* and *v2*
+* *v4* has around the same number of steps for the robot as *v3* but with the calculation speed of *v2*
--- pathfinder/src/grassfire.rs
+++ pathfinder/src/grassfire.rs
@@ -56,7 +56,7 @@ pub fn v1(area: &mut [u64], rows: &usize, cols: &usize, start:(usize, usize), en
 	}
 	println!("Grassfire-V1 Calc: {:.6?}", benchmark.elapsed());
 
-	find_path("Grassfire-V1", start, area, rows, cols)
+	find_path("Grassfire-V1", start, area, rows, cols, false)
 }
 
 /// Use the Grassfire algorithm to calculate the optimal way fro the start to the end.
@@ -122,7 +122,7 @@ pub fn v2(area: &mut [u64], rows: &usize, cols: &usize, start:(usize, usize), en
 	}
 	println!("Grassfire-V2 Calc: {:.6?}", benchmark.elapsed());
 
-	find_path("Grassfire-V2", start, area, rows, cols)
+	find_path("Grassfire-V2", start, area, rows, cols, false)
 }
 
 /// Use the Grassfire algorithm to calculate the optimal way fro the start to the end.
@@ -195,7 +195,74 @@ pub fn v3(area: &mut [u64], rows: &usize, cols: &usize, start:(usize, usize), en
 	}
 	println!("Grassfire-V3 Calc: {:.6?}", benchmark.elapsed());
 
-	find_path("Grassfire-V3", start, area, rows, cols)
+	find_path("Grassfire-V3", start, area, rows, cols, false)
+}
+
+
+/// Use the Grassfire algorithm to calculate the optimal way fro the start to the end.
+///
+/// Version 4: Hold the last processed fields in memory to not repeatedly go through the whole matrix
+///            use the 4-Neighborhood for calculation but for the path finding the 8-Neighborhood
+///
+/// # Arguments
+///
+/// * `area` - The play field as a one-dimensional vector
+/// * `rows` - number of rows
+/// * `cols` - number of cols
+/// * `start` - start position (row, col)
+/// * `end` - end position (row, col)
+///
+/// # Result
+///
+/// A Vector of tuples where each tuple represents a waypoint
+pub fn v4(area: &mut [u64], rows: &usize, cols: &usize, start:(usize, usize), end:(usize, usize)) -> Vec<(usize, usize)> {
+	let benchmark = Instant::now();
+
+	// Fill the start position with 1
+	area[(end.1 * rows) + end.0] = 1;
+
+	// Optimization: This vector holds the fields which has been processed in the last round
+	// Based on these the neighbours can be calculated, starting on the end
+	let mut last_fields: Vec<(usize, usize)> = vec![end];
+
+	// run as long as we have not reached the start field
+	// or as long as we have neighbours to calculate
+	loop {
+		let mut changed = false;
+
+		// Calculate the fields to calculate as next
+		let mut next_fields: Vec<(usize, usize)> = vec![];
+		last_fields.iter()
+			.for_each(|field| {
+				let row_next = match field.0 + 1 { row if &row < rows => Some((field.0 + 1, field.1)),     _ => None };
+				let row_prev = match field.0     { row if row > 0     => Some((field.0 - 1, field.1)),     _ => None };
+				let col_next = match field.1 + 1 { col if &col < cols => Some((field.0,     field.1 + 1)), _ => None };
+				let col_prev = match field.1     { col if col > 0     => Some((field.0,     field.1 - 1)), _ => None };
+
+				let value = area[(field.1 * rows) + field.0] + 1;
+				let mut check_field = |field: Option<(usize, usize)>| {
+					if let Some(fld) = field {
+						if area[(fld.1 * rows) + fld.0] == 0 {
+							next_fields.push(fld);
+							area[(fld.1 * rows) + fld.0] = value;
+							changed = true;
+						}
+					};
+				};
+				check_field(row_next);
+				check_field(row_prev);
+				check_field(col_next);
+				check_field(col_prev);
+			});
+		last_fields = next_fields.clone();
+
+		// This is not working in the while loop
+		if !changed { println!("Grassfire-V2 Calc: No conneciton possible"); break; }
+		if area[(start.1 * rows) + start.0] != 0 || last_fields.is_empty() { break; }
+	}
+	println!("Grassfire-V4 Calc: {:.6?}", benchmark.elapsed());
+
+	find_path("Grassfire-V4", start, area, rows, cols, true)
 }
 
 /// This is a generic function to finally findthe path from the start to the end
@@ -209,11 +276,12 @@ pub fn v3(area: &mut [u64], rows: &usize, cols: &usize, start:(usize, usize), en
 /// * `area` - Calculated area
 /// * `rows` - Number of rows
 /// * `cols` - Number of columns
+/// * `eight` - Check for 8-Neighborhood even if it was calculated in a 4-Neighborhood
 ///
 /// # Result:
 ///
 /// A Vector of tuples with usize values identifying the x/y position in the area
-pub fn find_path(algorithm: &str, start: (usize, usize), area: &mut [u64], rows: &usize, cols: &usize) -> Vec<(usize, usize)> {
+pub fn find_path(algorithm: &str, start: (usize, usize), area: &mut [u64], rows: &usize, cols: &usize, eight: bool) -> Vec<(usize, usize)> {
 	let benchmark = Instant::now();
 
 	let mut row_num = start.0;
@@ -242,10 +310,11 @@ pub fn find_path(algorithm: &str, start: (usize, usize), area: &mut [u64], rows:
 		let row_prev_down = if row_next.is_some() && col_prev.is_some() { Some(( row_num + 1, col_num - 1 )) } else { None };
 		let row_prev_up =   if row_prev.is_some() && col_prev.is_some() { Some(( row_num - 1, col_num - 1 )) } else { None };
 
-		let value = cell_value - 1;
+		// Closure to check a value for a lower value than an other one
 		let mut check_neighbor = |field: Option<(usize, usize)>| {
 			if let Some(fld) = field {
-				if area[(fld.1 * rows) + fld.0] == value {
+				let fld_val = area[(fld.1 * rows) + fld.0];
+				if fld_val > 0 && fld_val < cell_value {
 					row_num = fld.0;
 					col_num = fld.1;
 					return true;
@@ -254,8 +323,21 @@ pub fn find_path(algorithm: &str, start: (usize, usize), area: &mut [u64], rows:
 			false
 		};
 
-		// Try the neighbors, direct connected (up/down/left/right) are preferred to the edges
-		if !check_neighbor(col_prev) &&
+		match eight {
+			// Try the edge-neighbors before the direct connected (up/down/left/right)
+			true => if !check_neighbor(row_prev_up) &&
+				!check_neighbor(row_prev_down) &&
+				!check_neighbor(row_next_up) &&
+				!check_neighbor(row_next_down) &&
+				!check_neighbor(col_prev) &&
+				!check_neighbor(col_next) &&
+				!check_neighbor(row_prev) &&
+				!check_neighbor(row_next) {
+				break;
+			},
+
+			// Try the direct connected neighbors (up/down/left/right) before the edges
+			false => if !check_neighbor(col_prev) &&
 			 !check_neighbor(col_next) &&
 			 !check_neighbor(row_prev) &&
 			 !check_neighbor(row_next) &&
@@ -263,7 +345,8 @@ pub fn find_path(algorithm: &str, start: (usize, usize), area: &mut [u64], rows:
 			 !check_neighbor(row_prev_down) &&
 			 !check_neighbor(row_next_up) &&
 			 !check_neighbor(row_next_down) {
-			break;
+				break;
+			}
 		}
 	}
 	println!("{} Path-Calculation: {:.6?}", algorithm, benchmark.elapsed());
